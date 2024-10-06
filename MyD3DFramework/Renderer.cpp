cfc5083c923b012633f4341f0d2b903639820c06
CrabMan <alsxm0010@gmail.com>
2024-10-06 11:33:51 +0900

    깊이 스텐실 버퍼

--- MyD3DFramework/Renderer.cpp
+++ MyD3DFramework/Renderer.cpp
@@ -282,13 +282,23 @@ void Renderer::InitializeRenderResoucre()
 	{
 		D3D11_DEPTH_STENCIL_DESC desc = {};
 
-		desc.DepthEnable = true;
-		desc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
-		desc.DepthFunc = D3D11_COMPARISON_LESS;
+		desc.DepthEnable = true;  
+		desc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL; 
+		desc.DepthFunc = D3D11_COMPARISON_LESS; 
 
-		desc.StencilEnable = true;
-		desc.StencilReadMask = 0xff;
-		desc.StencilWriteMask = 0xff;
+		desc.StencilEnable = true; 
+		desc.StencilReadMask = 0xff;  
+		desc.StencilWriteMask = 0xff; 
+
+		desc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP; 
+		desc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;  
+		desc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP; 
+		desc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS; 
+
+		desc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP; 
+		desc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR; 
+		desc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP; 
+		desc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS; 
 
 		HRESULT hr = m_device->CreateDepthStencilState(&desc, m_depthStencilState.GetAddressOf());
 		CHECK_FAILED(hr);
